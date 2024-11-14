baff0caab98caa4145d583a422a86cbc085df9d6
prabalbhandary <prabalbhandary97@gmail.com>
2024-11-14 12:04:18 +0545

    Logo Link

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const AuthLayouts = ({children}) => {
   return (
     <>
         <header className='flex justify-center items-center py-3 h-20 shadow-md bg-white'>
+            <Link to="/email">
             <img 
               src={logo}
               alt='logo'
               width={180}
               height={60}
             />
+            </Link>
         </header>
 
         { children }
