f871dc209a3bb25ab9cafc08861478ea76a8ba6b
prabalbhandary <prabalbhandary97@gmail.com>
2024-11-17 05:06:13 +0545

    Cors

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ const cookieParser = require("cookie-parser");
 // file imports
 const connectDB = require("./config/db");
 const router = require("./routes/index");
-const {app, socket} = require("./socket/index");
+const { app, socket } = require("./socket/index");
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
 app.use(morgan("dev"));
 
 // variables
@@ -29,5 +29,8 @@ app.use("/api", router);
 
 // server listening
 app.listen(port, () => {
-    console.log(`Server is running in ${mode} mode on http://localhost${port}`.bgMagenta.white);
+  console.log(
+    `Server is running in ${mode} mode on http://localhost${port}`.bgMagenta
+      .white
+  );
 });
